7fecfbfaca552daf0e5abd539edcf20a65b79444
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-14 16:54:40 +0000

    feat(rich text): add block button component

---
+++ components/RichTextEditor/BlockButton/BlockButton.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Editor, Transforms } from 'slate';
+import { useSlate } from 'slate-react';
+import Button from '../IconButton';
+import { Element as SlateElement } from 'slate';
+
+const LIST_TYPES = ['numbered-list', 'bulleted-list'];
+const TEXT_ALIGN_TYPES = ['left', 'center', 'right', 'justify'];
+
+const BlockButton = ({ format, icon }) => {
+  const Icon = icon;
+  const editor = useSlate();
+  return (
+    <Button
+      active={isBlockActive(editor, format, TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type')}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        toggleBlock(editor, format);
+      }}
+    >
+      <Icon />
+    </Button>
+  );
+};
+
+const isBlockActive = (editor: Editor, format: string, blockType = 'type') => {
+  const { selection } = editor;
+  if (!selection) return false;
+
+  const [match] = Array.from(
+    Editor.nodes(editor, {
+      at: Editor.unhangRange(editor, selection),
+      match: (n) => !Editor.isEditor(n) && SlateElement.isElement(n) && n[blockType] === format,
+    })
+  );
+
+  return !!match;
+};
+
+const toggleBlock = (editor: Editor, format: string) => {
+  const isActive = isBlockActive(
+    editor,
+    format,
+    TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
+  );
+  const isList = LIST_TYPES.includes(format);
+
+  Transforms.unwrapNodes(editor, {
+    match: (n) =>
+      !Editor.isEditor(n) &&
+      SlateElement.isElement(n) &&
+      LIST_TYPES.includes(n.type) &&
+      !TEXT_ALIGN_TYPES.includes(format),
+    split: true,
+  });
+  let newProperties: Partial<SlateElement & { [key: string]: any }>;
+  if (TEXT_ALIGN_TYPES.includes(format)) {
+    newProperties = {
+      align: isActive ? undefined : format,
+    };
+  } else {
+    newProperties = {
+      type: isActive ? 'paragraph' : isList ? 'list-item' : format,
+    } as unknown as SlateElement;
+  }
+  Transforms.setNodes<SlateElement>(editor, newProperties);
+
+  if (!isActive && isList) {
+    const block = { type: format, children: [] };
+    Transforms.wrapNodes(editor, block as unknown as SlateElement);
+  }
+};
+
+export default BlockButton;
